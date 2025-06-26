6cbbbf622ca74619ad534810f28073ec1744d2bf
Maria <maria.evm@outlook.com>
2025-06-26 22:51:57 +0100

    changed asterix to wildcard name

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get('/{*any}', (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
